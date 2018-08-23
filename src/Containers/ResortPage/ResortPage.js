f650316d96231b6891a85a49365766253d7b8be2
Reagan Ibbotson <reagan_ib@hotmail.com>
2018-08-23 22:05:26 +1000

    Resort Page

--- src/Containers/ResortPage/ResortPage.js
+++ src/Containers/ResortPage/ResortPage.js
@@ -5,7 +5,7 @@ class ResortPage extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
-			data:[],
+			data:'',
 		}
 	}
 
@@ -16,18 +16,28 @@ class ResortPage extends React.Component{
 			const displayResort = respJson.filter((resort)=>{
 				return resort.name === this.props.resortSelection
 			})
-			this.setState({data: displayResort})
-			console.log(displayResort);
-			
+			this.setState({data: displayResort[0]})
+			console.log(displayResort[0]);
 		})
 	}
 
 	render(){
 		return(
-			<div>
+			<div className='flex-centred'>
 				<h1>
-					{this.state.data.id}
+					{this.state.data.name}
 				</h1>
+				<div className='grid'>
+					<div className='mountain-img full-grid-row row2'>
+						<p>Insert image here?</p>
+					</div>
+					<div className='centred-grid-row row3'>
+						<p>Insert description/blurb here.</p>
+					</div>
+					<div className='review-system-wrapper centred-grid-row row4'>
+						<p>Insert review system component here.</p>
+					</div>
+				</div>
 			</div>
 		);
 	}
