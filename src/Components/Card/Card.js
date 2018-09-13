2ff4a7bc1d6cf7a9470d1448624a4d60bb87a27e
Reagan Ibbotson <ReaganI@pvsoz.com.au>
2018-09-13 14:35:24 +1000

    Things

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -2,27 +2,21 @@ import React from 'react';
 import './Card.css';
 
 function Card(props){
-	let count = 0;
 
 	const CountryList = props.Data.map((card, i)=>{
-		//tick count
-		count++
 
-		//remove the if statement when we're mapping the actual list
-		//if(count<=limit){
-			return(
-				<div className={`card`} onClick={props.changeSelection} key={i} id={card.region}>
-					<div className='card-title'>
-						<p>{card.region}</p>
-					</div>
-					<div className='card-content'>
-						<p>{card.region}</p>
-					</div>
+		return(
+			<div className={`card`} onClick={props.changeSelection} key={i} id={card.region}>
+				<div className='card-title'>
+					<p>{card.region}</p>
 				</div>
-			);
-		//}
+				<div className='card-content'>
+					<p>{card.region}</p>
+				</div>
+			</div>
+		);
 	});
-
+	
 	return CountryList
 }
 
